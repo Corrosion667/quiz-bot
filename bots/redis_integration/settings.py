00c7e1cbfef52848d2ec4391e923a6773f0b223d
Artem Nechai <lenikol95@mail.ru>
2022-06-15 23:26:09 +0300

    update quiz upload algorythm

--- bots/redis_integration/settings.py
+++ bots/redis_integration/settings.py
@@ -1,9 +1,11 @@
 """Redis settings of the project."""
 
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-QUIZ_TASKS_DIR = os.path.join(BASE_DIR, 'quiz_tasks')
+QUIZ_TASKS_DIR = os.path.join(
+    Path(__file__).absolute().parent.parent, 'quiz_tasks',
+)
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
--- bots/redis_integration/upload_quiz.py
+++ bots/redis_integration/upload_quiz.py
@@ -1,19 +1,22 @@
 """Module for creating quiz questions and answers pairs in Redis from quiz fixture files."""
 
 import re
+from dotenv import load_dotenv
+import time
+from os import listdir
+from os.path import join
 
 from redis import Redis
 
-from bots.redis_integration.settings import REDIS_HOST, REDIS_PORT, TASKS_DATABASE
+from bots.redis_integration.settings import QUIZ_TASKS_DIR, REDIS_HOST, REDIS_PORT, TASKS_DATABASE
 
 
-def parse_quiz_file(path_to_file):
-    """Parse quiz file and create records in Redis of questions and answers.
+def parse_quiz_file(path_to_file, pipe):
+    """Parse quiz file and append creation of questions and answers to in Redis of pipeline.
 
     Args:
         path_to_file: path to file with quiz content.
     """
-    client = Redis(host=REDIS_HOST, port=REDIS_PORT, db=TASKS_DATABASE)
     with open(path_to_file, encoding='KOI8-R') as quiz_file:
         quiz_content = quiz_file.read()
     split_content = quiz_content.split('\n\n')
@@ -24,4 +27,20 @@ def parse_quiz_file(path_to_file):
         re.sub('^Ответ.+\n', '', split) for split in split_content if 'Ответ' in split
     ]
     for question, answer in zip(questions, answers):
-        client.set(question, answer)
+        pipe.set(question, answer)
+
+
+def load_quiz_tasks(pipe):
+    """Script for loading quiz questions and answers to Redis from all quiz files."""
+    quiz_files_paths = [join(QUIZ_TASKS_DIR, file_name) for file_name in listdir(QUIZ_TASKS_DIR)]
+    for path in quiz_files_paths:
+        parse_quiz_file(path, pipe)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    redis_client = Redis(host=REDIS_HOST, port=REDIS_PORT, db=TASKS_DATABASE, decode_responses=True)
+    pipe = redis_client.pipeline()
+    start_time = time.time()
+    load_quiz_tasks(pipe)
+    pipe.execute()

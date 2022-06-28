30e646efd3690d9ac0bd3c1e987b7cb78afdf148
Артём Нечай <a.nechai@okenit.ru>
2022-06-28 16:06:18 +0300

    vk handle give up and score request

--- bots/vkontakte_bot.py
+++ bots/vkontakte_bot.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 from redis import Redis
@@ -11,7 +12,7 @@ from vk_api.longpoll import VkEventType, VkLongPoll
 from vk_api.utils import get_random_id
 from vk_api.vk_api import VkApiMethod
 
-from bots.settings import TASKS_DATABASE, USERS_DATABASE, ButtonText
+from bots.settings import SCORE_TEXT, TASKS_DATABASE, USERS_DATABASE, ButtonText
 
 
 def create_keyboard() -> str:
@@ -49,9 +50,16 @@ def interact_longpoll(vk_long_poll: VkLongPoll, vk_api_method: VkApiMethod) -> N
                 user_id=user_id, message=question, random_id=get_random_id(), keyboard=keyboard,
             )
         if event.message == ButtonText.GIVE_UP.value:
-            handle_give_up_request()
+            answer = handle_give_up_request(databases=databases, user_id=user_id)
+            if answer:
+                vk_api_method.messages.send(
+                    user_id=user_id, message=answer, random_id=get_random_id(), keyboard=keyboard,
+                )
         if event.message == ButtonText.SCORE.value:
-            handle_score_request()
+            score_text = handle_score_request(databases=databases, user_id=user_id)
+            vk_api_method.messages.send(
+                user_id=user_id, message=score_text, random_id=get_random_id(), keyboard=keyboard,
+            )
         handle_solution_attempt()
 
 
@@ -81,14 +89,48 @@ def get_new_question(databases: dict, user_id: int) -> str:
     return question
 
 
-def handle_give_up_request() -> None:
-    """Docstring."""
-    pass  # noqa: WPS420
+def handle_give_up_request(databases: dict, user_id: int) -> Optional[str]:
+    """Get answer for last asked question to user and increase give up counter.
+
+    Args:
+        databases: connectors to redis databases (users and tasks).
+        user_id: id of the user in Vkontakte.
+
+    Returns:
+        Answer for last asked question or None if there haven't been any questions for user yet.
+    """
+    users_db = databases['users']
+    user_id_db = f'user_vk_{user_id}'
+    saved_user_data = users_db.get(user_id_db)
+    if not saved_user_data:
+        return None
+    decoded_user_data = json.loads(saved_user_data)
+    decoded_user_data['give_up'] += 1
+    users_db.set(user_id_db, json.dumps(decoded_user_data))
+    tasks_db = databases['tasks']
+    asked_question = decoded_user_data['last_asked_question']
+    return tasks_db.get(asked_question)
 
 
-def handle_score_request() -> None:
-    """Docstring."""
-    pass  # noqa: WPS420
+def handle_score_request(databases: dict, user_id: int) -> str:
+    """Get user's score: successes and give ups.
+
+    Args:
+        databases: connectors to redis databases (users and tasks).
+        user_id: id of the user in Vkontakte.
+
+    Returns:
+        Score text message for user.
+    """
+    users_db = databases['users']
+    user_id_db = f'user_vk_{user_id}'
+    saved_user_data = users_db.get(user_id_db)
+    if not saved_user_data:
+        return SCORE_TEXT.format(successes=0, give_ups=0)
+    decoded_user_data = json.loads(saved_user_data)
+    successes = decoded_user_data['success']
+    give_ups = decoded_user_data['give_up']
+    return SCORE_TEXT.format(successes=successes, give_ups=give_ups)
 
 
 def handle_solution_attempt() -> None:

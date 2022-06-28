941b495661a3c18c001cd383e9ebc1b5b682c004
Артём Нечай <a.nechai@okenit.ru>
2022-06-28 17:13:09 +0300

    put checking answer in new file because it is common func

--- bots/telegram_bot.py
+++ bots/telegram_bot.py
@@ -3,9 +3,6 @@
 import json
 import logging
 import os
-import re
-import string
-from difflib import SequenceMatcher
 from typing import Optional
 
 from dotenv import load_dotenv
@@ -15,38 +12,15 @@ from telegram.ext import (
     CallbackContext, CommandHandler, ConversationHandler, Filters, MessageHandler, Updater,
 )
 
+from bots.check_answer import is_correct_answer
 from bots.settings import (
-    CANCEL_TEXT, GIVE_UP, GREETING_TG, HELP_TEXT, NEXT, RIGHT_ANSWER, SCORE_TEXT,
-    STRING_EQUALITY_RATIO, TASKS_DATABASE, USERS_DATABASE, WRONG_ANSWER, ButtonText,
+    CANCEL_TEXT, GIVE_UP, GREETING_TG, HELP_TEXT, NEXT, RIGHT_ANSWER, SCORE_TEXT, TASKS_DATABASE,
+    USERS_DATABASE, WRONG_ANSWER, ButtonText,
 )
 
 logger = logging.getLogger(__name__)
 
 CHOOSING, CHECK_ANSWER = range(2)
-
-
-def is_correct_answer(users_answer: str, correct_answer: str) -> bool:
-    """Check if answer given by user is correct.
-
-    Comments in brackets [] or () aren't taken into consideration when counting string match ratio.
-    Punctuation symbols are excluded from calculation too.
-
-    Args:
-        users_answer: answer received from user.
-        correct_answer: answer from the database.
-
-    Returns:
-        True if answer is correct else False.
-    """
-    commentless_answer = re.sub(r'\s?\(.+\)|\s?\[.+]', '', correct_answer)
-    filtered_correct_answer = ''.join(
-        [symbol for symbol in commentless_answer.lower() if symbol not in string.punctuation],
-    )
-    filtered_users_answer = ''.join(
-        [symbol for symbol in users_answer.lower() if symbol not in string.punctuation],
-    )
-    matching_ratio = SequenceMatcher(None, filtered_correct_answer, filtered_users_answer).ratio()
-    return matching_ratio >= STRING_EQUALITY_RATIO
 
 
 def start(update: Update, context: CallbackContext) -> Optional[int]:
